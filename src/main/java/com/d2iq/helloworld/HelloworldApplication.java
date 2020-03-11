f3db5497738e21c681403c8e9fbd469e338df604
Jeff <jeffhsu@tw.ibm.com>
2020-03-11 17:20:14 +0800

    test

--- src/main/java/com/d2iq/helloworld/HelloworldApplication.java
+++ src/main/java/com/d2iq/helloworld/HelloworldApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloworldApplication {
 
     public static void main(String[] args) {
+	String tmp = "";
         SpringApplication.run(HelloworldApplication.class, args);
     }
 }
